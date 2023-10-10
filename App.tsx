30190e46619d57e2efa8e1b732968340d1aca869
Linmy26 <linhhtmps24266@fpt.edu.vn>
2023-10-10 11:39:34 +0700

    fonts
    fonts

--- App.tsx
+++ App.tsx
@@ -1,118 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
+ 
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { fontFamily } from './assets/fonts';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView >
+    <Text style={styles.black}>Huỳnh Thị Mỹ Linh </Text>
+    <Text style={styles.bold}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.extraLight}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.extraBold}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.italic}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.light}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.medium}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.regular}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.semibold}>Huỳnh Thị Mỹ Linh  </Text>
+    <Text style={styles.thin}>Huỳnh Thị Mỹ Linh  </Text>
+
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  black:{
+    fontFamily:fontFamily.Black,
+    fontSize:25,
+    color:'black'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  bold:{
+    fontFamily:fontFamily.Bold,
+    fontSize:25
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  extraLight:{
+    fontFamily:fontFamily.ExtraLight,
+    fontSize:25,
   },
-  highlight: {
-    fontWeight: '700',
+  extraBold:{
+    fontFamily:fontFamily.ExtraBold,
+    fontSize:25
   },
+  italic:{
+    fontFamily:fontFamily.Italic,
+    fontSize:25
+  },
+  light:{
+    fontFamily:fontFamily.Light,
+    fontSize:25
+  },
+  thin:{
+    fontFamily:fontFamily.Thin,
+    fontSize:25
+  },
+  medium:{
+    fontFamily:fontFamily.Medium,
+    fontSize:25
+  },
+  regular:{
+    fontFamily:fontFamily.Regular,
+    fontSize:25
+  },
+  semibold:{
+    fontFamily:fontFamily.Semibold,
+    fontSize:25
+  }
 });
 
 export default App;
---
+++ assets/fonts/index.ts
@@ -0,0 +1,12 @@
+export const fontFamily = {
+    Black: 'Poppins-Black',
+    Bold: 'Poppins-Bold',
+    ExtraLight: 'Poppins-ExtraLight',
+    ExtraBold: 'Poppins-ExtraBold',
+    Italic: 'Poppins-Italic',
+    Light: 'Poppins-Light',
+    Medium: 'Poppins-Medium',
+    Regular: 'Poppins-Regular',
+    Semibold: 'Poppins-SemiBold',
+    Thin: 'Poppins-Thin',
+  };
